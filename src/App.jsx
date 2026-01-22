da40abe1f568f75dbb14abd51740421a52cfa801
ayman <aemanharde@gmail.log>
2026-01-22 16:36:31 +0300

    fixing names and set condition to show text

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import "./App.css";
 export default function App() {
   return (
     <div>
-      {/* <TextExpander>
+      <TextExpander>
         Space travel is the ultimate adventure! Imagine soaring past the stars
         and exploring new worlds. It's the stuff of dreams and science fiction,
         but believe it or not, space travel is a real thing. Humans and robots
         are constantly venturing out into the cosmos to uncover its secrets and
         push the boundaries of what's possible.
-      </TextExpander> */}
+      </TextExpander>
 
       <TextExpander
         collapsedNumWords={20}
@@ -25,37 +25,39 @@ export default function App() {
         foot on the moon or when rovers were sent to roam around on Mars.
       </TextExpander>
 
-      {/* <TextExpander expanded={true} className="box">
+      <TextExpander expanded={true} className="box">
         Space missions have given us incredible insights into our universe and
         have inspired future generations to keep reaching for the stars. Space
         travel is a pretty cool thing to think about. Who knows what we'll
         discover next!
-      </TextExpander> */}
+      </TextExpander>
     </div>
   );
 }
 
 function TextExpander({
-  children,
+  children, // FULL TEXT
   collapsedNumWords = 10,
-  collapseButtonText = "Show more",
+  expandButtonText = "Show More",
+  collapseButtonText = "Show less",
   buttonColor = "#000",
+  expanded = false,
 }) {
-  const [showText, setShowText] = useState(false);
+  const [showText, setShowText] = useState(expanded);
 
   const strLength = children.split(" ").length; // LENGTH OF STRING (WORD)
-  const wordAt = children.split(" ").slice(0, 5);
+  const cutText = children.split(" ").slice(0, collapsedNumWords);
 
-  let concat = "";
-  wordAt.map((word) => {
-    concat += word + " ";
+  let collapsedText = "";
+  cutText.map((word) => {
+    collapsedText += word + " ";
   });
 
   return (
     <div>
       <div>
-        {concat}
-        <button>SHOW</button>
+        {showText ? children : collapsedText}
+        <button>{expandButtonText}</button>
       </div>
       <br />
     </div>
